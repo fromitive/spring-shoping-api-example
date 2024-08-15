d03c829d5618de93965d33e591dcf14051289e73
fromitive <fromitive@gmail.com>
2024-08-15 15:32:11 +0900

    init project

---
+++ src/main/java/com/example/spring_shoping_api_example/SpringShopingApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_shoping_api_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopingApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopingApiExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_shoping_api_example/SpringShopingApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_shoping_api_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopingApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
